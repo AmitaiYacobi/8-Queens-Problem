d06bdd5c8630df6a685f990908ff44a47e50ca4e
roneliav <roneliav1@gmail.com>
2022-04-25 20:02:15 +0300

    add altisim config and fix selection

--- genetic_alogrithm.py
+++ genetic_alogrithm.py
@@ -53,27 +53,39 @@ def selection(population_scores_dict):
     pick = random.uniform(0, sum_of_scores)
     current = 0
     for chromosome, score in population_scores_dict.items():
-        if score == 28: continue
         current += score
         if current > pick:
             return chromosome
 
+def singlepoint_crossover(parent1, parent2, rate="random"):
+    if rate == "random":
+        single_point = random.randint(0, len(parent1))
+    else:
+        single_point = rate / len(parent1)
 
-def crossover(parent1, parent2):
-    single_point = random.randint(0, 7)
     parent1 = list(parent1)
     parent2 = list(parent2)
     offspring1 = parent1[:single_point] + parent2[single_point:]
     offspring2 = parent2[:single_point] + parent1[single_point:]
     return tuple(offspring1), tuple(offspring2)
 
+def crossover(parent1, parent2, crossove_type=singlepoint_crossover, rate="random"):
+    """
 
-def mutation(offspring1, offspring2):
+    :param parent1: first parent (tuple)
+    :param parent2: second parent (tuple)
+    :param crossove_type: function for crossover. get arguments: parent1, parent2, crossover rate
+    :param rate: rate for crossover
+    :return: two children
+    """
+    return crossove_type(parent1, parent2, rate=rate)
+
+
+def mutation(offspring1, offspring2, rate=0.4):
+    offspring1 = list(offspring1)
+    offspring2 = list(offspring2)
     for i in range(len(offspring1)):
-        prop = random.randint(0, 7)
-        offspring1 = list(offspring1)
-        offspring2 = list(offspring2)
-        if prop < 0.4:
+        if random.random() < rate: # to do mutation
             offspring1[i] = random.randint(0, 7)
             offspring2[i] = random.randint(0, 7)
     return tuple(offspring1), tuple(offspring2)
@@ -83,20 +95,44 @@ def create_population_scores_dict(population, scores):
     return {tuple(population[i]): scores[i] for i in range(len(scores))}
 
 
-def run_algorithm(population_size=10):
+def alitism(popul_scores_dict, p=0.1):
+    """
+
+    :param popul_scores_dict: key: chromosom, value: score
+    :param p: float (percentage) how many besh chromosoms to pass to the next generation.
+    :return: list of best chromosoms
+    """
+    n_best = int(p * len(popul_scores_dict))
+    sorted_popul = sorted(popul_scores_dict.items(), key=lambda item: item[1], reverse=True)
+    best = sorted_popul[:n_best]
+    best_rep = [s[0] for s in best] # save the representation of each solution from the best ones
+    return best_rep
+
+
+def num_of_solutions(population_scores_dict):
+    solutions = 0
+    for c, v in population_scores_dict.items():
+        if v == 28:
+            solutions += 1
+    return solutions
+
+
+def run_algorithm(population_size, crossover_type, crossover_rate, mutation_rate, p_alitism):
     generation = 0
     population = generate_population(population_size)
     scores = population_fitness(population)
     population_scores_dict = create_population_scores_dict(population, scores)
     best_score = max(population_scores_dict.values())
 
-    while best_score != 28:
+    while num_of_solutions(population_scores_dict) < N_SOLUTIONS: # best_score != 28:
         new_population = []
-        for _ in range(int(len(population) / 2)):
+        new_population.extend(alitism(population_scores_dict, p=p_alitism))
+        remain = len(population) - len(new_population)
+        for _ in range(remain // 2): # run all population except the alitism we pass
             parent1 = selection(population_scores_dict)
             parent2 = selection(population_scores_dict)
-            offspring1, offspring2 = crossover(parent1, parent2)
-            offspring1, offspring2 = mutation(offspring1, offspring2)
+            offspring1, offspring2 = crossover(parent1, parent2, crossove_type=crossover_type, rate=crossover_rate)
+            offspring1, offspring2 = mutation(offspring1, offspring2, rate=mutation_rate)
             new_population.append(offspring1)
             new_population.append(offspring2)
             if len(new_population) == population_size:
@@ -115,13 +151,33 @@ def run_algorithm(population_size=10):
 
 
 if __name__ == "__main__":
-    population, num_of_generations = run_algorithm(population_size=1000)
-    counter = 0
-    for chromosome, score in population.items():
-        if score == 28:
-            counter += 1
+    config = {
+        "population_size" : 1000,
+        "crossover_type": singlepoint_crossover,
+        "crossover_rate": "random",
+        "mutation_rate": 0.2,
+        "p_alitism": 0.2
+    }
+    N_SOLUTIONS = 10
+    solutions_counter = 0
+    solutions = []
+    iteration = 0
+    sum_generations = 0
+    while solutions_counter < N_SOLUTIONS:
+        population, num_of_generations = run_algorithm(**config)
+        iteration += 1
+        sum_generations += num_of_generations
+        print("finish one running")
+        for chromosome, score in population.items():
+            if score == 28:
+                solutions_counter += 1
+                solutions.append(chromosome)
 
-    print("\n##################################")
-    print(
-        f"solution found after {num_of_generations} generations")
-    print("##################################\n")
+    print(f"sum of generations: {sum_generations}, avg generations: {sum_generations/iteration}")
+    print("solutoins:")
+    print(solutions)
+
+    # print("\n##################################")
+    # print(
+    #     f"solution found after {num_of_generations} generations")
+    # print("##################################\n")
